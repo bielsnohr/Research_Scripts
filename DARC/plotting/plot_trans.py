b76c73a22521d03e072fb619afe1fe3cd9668593
bluteau <matthew.bluteau@gmail.com>
2014-06-06 11:33:06 +0100

    Added functionality for plotting omega files and dealing with AS results correctly

--- DARC/plotting/plot_trans.py
+++ DARC/plotting/plot_trans.py
@@ -20,6 +20,7 @@ import math
 import datetime as dt
 import glob
 import argparse as ap
+import re
 
 
 def main(argv):
@@ -34,7 +35,7 @@ def main(argv):
     parser.add_argument('trans', nargs='+', help='the list list of transitions'
                         ' specified by pairs of numbers, lower level then '
                         'upper level')
-    parser.add_argument('-b', action='store_true', help='flag'
+    parser.add_argument('-b', action='store_true', default=False, help='flag'
                         ' to plot Burgess-Tully scaled data present in the '
                         'current directory instead of the normal data. Files '
                         'with .burg extensions must contain this data.')
@@ -58,7 +59,7 @@ def main(argv):
 
     # Level nomenclature dict; should make this a bit more portable at some
     # point
-    lev = {'1':'3d^{10}\!4s^2 \ ^1\!S_0^{\circ}', '275':\
+    lev = {'1':'3d^{10}\!4s^2 \ ^1\!S_0', '275':\
             '3d^{9}\!4s^2\!4f \ \ ^3\!D_1^{\circ}', '290':\
             '3d^{9}\!4s^2\!4f \ \ ^1\!P_1^{\circ}', '295':\
             '3d^{9}\!4s^2\!4f \ \ ^3\!P_1^{\circ}', '2':\
@@ -69,12 +70,12 @@ def main(argv):
             '8':'3d^{10}\!4p^2 \ \ ^1\!D_2'}
     LOGY = args.logy
     LOGX = args.logx
-    typ = args.t
+    typ = args.t[0]
     if args.outfile == False:
         save = False
     else:
         save = True
-        outfile = args.outfile
+        outfile = args.outfile[0]
     x = args.trans
     if len(x) % 2 is not 0:
         raise Exception("You have given an odd number of levels."
@@ -82,6 +83,8 @@ def main(argv):
     trans = [x[(2 * i): (2 * i + 2)] for i in range(int(len(x) / 2))]
     #print(trans)
     fname = []
+    sty = ['-', '--', '-.', ':', '_', '.', '-x', '-*']
+    clr = ['b', 'r', 'g', 'k', 'y']
     for i in trans:
         try:
             [int(a) for a in i]
@@ -91,9 +94,15 @@ def main(argv):
             print("You entered: ", err.args)
             raise
         if args.b:
-            files = glob.glob('ups-' + i[0] + '_' + i[1] + '*' + '.burg')
+            if typ == 'ups':
+                files = glob.glob('ups-' + i[0] + '_' + i[1] + '-*' + '.burg')
+            else:
+                files = glob.glob('om-' + i[0] + '_' + i[1] + '-*' + '.burg')
         else:
-            files = glob.glob('ups-' + i[0] + '_' + i[1] + '*' + '.trns')
+            if typ == 'ups':
+                files = glob.glob('ups-' + i[0] + '_' + i[1] + '-*' + '.trns')
+            else:
+                files = glob.glob('om-' + i[0] + '_' + i[1] + '-*' + '.trns')
         print('Processing transition ', i)
         print('Files:')
         print(files)
@@ -102,9 +111,14 @@ def main(argv):
         title = 'Transition ' + i[0] + '-' + i[1] + ', $' + lev[i[0]]\
                 + '\\rightarrow' + lev[i[1]] + '$'
         if save:
-            fname.append('ups-' + i[0] + '_' + i[1] + '-' + outfile + '-'\
-                         + dt.date.today().isoformat().replace('-', '_') +\
-                         '.pdf')
+            if typ == 'ups':
+                fname.append('ups-' + i[0] + '_' + i[1] + '-' + outfile + '-'\
+                             + dt.date.today().isoformat().replace('-', '_') +\
+                             '.pdf')
+            else:
+                fname.append('om-' + i[0] + '_' + i[1] + '-' + outfile + '-'\
+                             + dt.date.today().isoformat().replace('-', '_') +\
+                             '.pdf')
         if LOGX:
             maxx = 0
             minx = 1e20
@@ -112,21 +126,33 @@ def main(argv):
             maxy = 0
             miny = 1e20
         count = 1
-        sty = ['-', '--', '-.', '-x', '-*']
-        for j in files:
+        for j in reversed(files):
             desc = j.split('-')[2]
             legend.append(desc)
             # Load the input files and plot the appropriate columns
             data = np.loadtxt(j)
+            # TO-DO: need to check the column assignments below
+            #print('args.b= ', args.b)
+            #print(data[0:-1, 1])
             if args.b:
-                x1 = data[:, 0]
-                y1 = data[:, 1]
+                if desc == 'AS_DW':
+                    x1 = data[:, 2]
+                    y1 = data[:, 3]
+                else:
+                    x1 = data[:, 0]
+                    y1 = data[:, 1]
             else:
                 if desc == 'AS_DW':
                     x1 = data[0:-1, 0]
+                    y1 = data[0:-1, 1]
+                    if typ == 'om':
+                        with open(j, 'r') as file:
+                            head = file.readline()
+                        a = float(re.sub(r'[^\d.]+', '', head.split('=')[1]))
+                        x1 = x1 + a
                 else:
                     x1 = data[0:-1, 1]
-                y1 = data[0:-1, 3]
+                    y1 = data[0:-1, 3]
             if LOGX:
                 maxx = max(maxx, np.max(x1))
                 minx = min(minx, np.min(x1))
@@ -135,7 +161,7 @@ def main(argv):
                 miny = min(miny, np.min(y1))
             ## Create the desired uncertainty region
             #err = 0.1 * y2
-            ax.plot(x1, y1, sty[count - 1])
+            ax.plot(x1, y1, clr[count - 1] + sty[count - 1])
             if count % 5 == 0:
                 count = 1
             else:
@@ -149,8 +175,12 @@ def main(argv):
             ax.set_xlim([xmin, xmax])
         if LOGY:
             ax.set_yscale('log')
+            #print(maxy, miny)
             ymax = maxy + 10 ** math.floor(math.log10(maxy))
-            ymin = miny - 10 ** math.floor(math.log10(miny))
+            if round(miny) == 0:
+                ymin = 0
+            else:
+                ymin = miny - 10 ** math.floor(math.log10(miny))
             ax.set_ylim([ymin, ymax])
         if typ == 'ups':
             if args.b:
